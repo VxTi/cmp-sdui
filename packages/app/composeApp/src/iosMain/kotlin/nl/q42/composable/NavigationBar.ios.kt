96dcb7648f0d910bb89415d1b8dd9eb8f861dd3f
Luca Warmenhoven <lucawarmenhoven@outlook.com>
2025-09-11 10:25:27 +0200

    Updated ios navigation bar (still broken)

--- packages/app/composeApp/src/iosMain/kotlin/nl/q42/composable/NavigationBar.ios.kt
+++ packages/app/composeApp/src/iosMain/kotlin/nl/q42/composable/NavigationBar.ios.kt
@@ -1,9 +1,11 @@
 package nl.q42.composable
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.interop.UIKitViewController
 import androidx.compose.ui.window.ComposeUIViewController
@@ -21,62 +23,61 @@ import platform.darwin.NSObject
 internal actual fun NavigationBar(controller: ViewController) {
     val tabs by controller.tabs.collectAsState()
     val selectedTabIndex by controller.selectedTabIndex.collectAsState()
+    val screen by controller.screen.collectAsState()
 
-    val tabBarController = remember {
-        // Create the TabBarController and its delegate only once
-        val delegate = object : NSObject(), UITabBarControllerDelegateProtocol {
-            override fun tabBarController(
-                tabBarController: UITabBarController,
-                didSelectViewController: UIViewController
-            ) {
-                // When a tab is selected in UIKit, update the state in Compose
-                controller.setSelectedTabIndex(tabBarController.selectedIndex.toInt())
-            }
-        }
-
-        UITabBarController().apply {
-            this.delegate = delegate
+    // Fetch the initial screen and tabs once
+    LaunchedEffect(Unit) {
+        if (screen == null) {
+            controller.fetchInitialScreen()
         }
     }
 
-    UIKitViewController(
-        factory = { tabBarController },
-        modifier = Modifier,
-        update = {
-            // The update block is called on every recomposition.
-            // This is the ideal place to sync the UIKit view with the Compose state.
-
-            // 1. Sync the selected tab index
-            // Check if the selected index in UIKit is different from our state
-            if (tabBarController.selectedIndex.toInt() != selectedTabIndex) {
-                tabBarController.setSelectedIndex(selectedTabIndex.toULong())
-            }
-
-            // 2. Sync the view controllers (tabs)
-            // Check if the number of tabs or their titles have changed
-            val currentViewControllers =
-                tabBarController.viewControllers?.mapNotNull { it as? UIViewController }
-                    ?: emptyList()
-            if (currentViewControllers.size != tabs.size || currentViewControllers.map { it.title } != tabs.map { it.title }) {
-
-                // If they have changed, create new view controllers
-                val newViewControllers = tabs.mapIndexed { index, tab ->
-                    // Use ComposeUIViewController to host Composable content
-                    ComposeUIViewController {
-                        controller.getScreenById(tab.screenId)?.let { screen ->
-                            DynamicContentList(screen.content, controller)
-                        }
-                    }.apply {
-                        this.title = tab.title
-                        this.tabBarItem = UITabBarItem(
-                            title = tab.title,
-                            image = null, // TODO: Add UIImage for icon
-                            tag = index.toLong()
-                        )
+    // Only render the tab bar if there are tabs and a screen is loaded
+    if (tabs.isNotEmpty() && screen != null) {
+        val tabBarController = remember {
+            UITabBarController().apply {
+                this.delegate = object : NSObject(), UITabBarControllerDelegateProtocol {
+                    override fun tabBarController(
+                        tabBarController: UITabBarController,
+                        didSelectViewController: UIViewController
+                    ) {
+                        controller.setSelectedTabIndex(tabBarController.selectedIndex.toInt())
                     }
                 }
-                tabBarController.setViewControllers(newViewControllers, animated = false)
             }
         }
-    )
+
+        UIKitViewController(
+            factory = { tabBarController },
+            modifier = Modifier,
+            update = {
+                // Sync selected tab index
+                if (tabBarController.selectedIndex.toLong() != selectedTabIndex.toLong()) {
+                    tabBarController.setSelectedIndex(selectedTabIndex.toULong())
+                }
+
+                // Sync the view controllers (tabs)
+                val currentViewControllers = tabBarController.viewControllers?.mapNotNull { it as? UIViewController } ?: emptyList()
+                if (currentViewControllers.size != tabs.size || currentViewControllers.map { it.title } != tabs.map { it.title }) {
+                    val newViewControllers = tabs.mapIndexed { index, tab ->
+                        ComposeUIViewController {
+                            // A screen composable that observes the current screen for the tab
+                            val currentScreenForTab by controller.screen.collectAsState()
+                            if (currentScreenForTab?.id == tab.screenId) {
+                                DynamicContentList(currentScreenForTab!!.content, controller)
+                            }
+                        }.apply {
+                            this.title = tab.title
+                            this.tabBarItem = UITabBarItem(
+                                title = tab.title,
+                                image = null, // TODO: Add UIImage for icon
+                                tag = index.toLong()
+                            )
+                        }
+                    }
+                    tabBarController.setViewControllers(newViewControllers, animated = false)
+                }
+            }
+        )
+    }
 }
